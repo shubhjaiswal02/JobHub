40bec3646e0be1f2c87b1b5cf449dc996449c0ad
shubh02 <shubhcodez@gmail.com>
2023-03-28 23:27:13 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from 'expo-router';
+
+import logo from "../assets/logo.png";
+import { Image } from 'react-native';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import {
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
+
+
+const Home = () => {
+    const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("")
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: "JobHub",
+                    headerTitleAlign: "center" 
+                    
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+                        searTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if(searchTerm){
+                                router.push(`/search/${searchTerm}`)
+                            }
+                        }}
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />                  
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
